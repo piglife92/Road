df1a67754e841d3b7117674838201c6ef2658437
Youngjae Park <54255680+piglife92@users.noreply.github.com>
2019-12-02 18:58:37 +0900

    Road 움직임구현
    움직임 구현완료

---
+++ Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float startPosition;
+    [SerializeField] float endPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -speed * Time.deltaTime, 0);
+        if (transform.position.y <= endPosition)
+        {
+            transform.Translate(0, -(endPosition - startPosition), 0);
+        }
+    }
+}
